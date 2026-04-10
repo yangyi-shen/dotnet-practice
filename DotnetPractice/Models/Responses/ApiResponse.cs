4200d219ef9211788289865c781d82edd4c747ba
Yang-Yi Shen <mrshenyangyi@gmail.com>
2026-04-10 16:50:02 +0700

    feat: cập nhật class api response

--- DotnetPractice/Models/Responses/ApiResponse.cs
+++ DotnetPractice/Models/Responses/ApiResponse.cs
@@ -8,6 +8,12 @@ namespace DotnetPractice.Models.DTOs
     public class ApiResponse<T>
     {
         public bool Status { get; set; }
-        public required T Data { get; set; }
+        public T Data { get; set; } = default!;
+
+        public ApiResponse(T data)
+        {
+            Status = true;
+            Data = data;
+        }
     }
 }
