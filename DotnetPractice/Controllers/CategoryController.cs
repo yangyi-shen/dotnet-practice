d1d504b06857431fc72e9f4d4dd2cd966c579e11
Yang-Yi Shen <mrshenyangyi@gmail.com>
2026-04-13 08:46:44 +0700

    feat: tạo những điểm cuối api cho category

--- DotnetPractice/Controllers/CategoryController.cs
+++ DotnetPractice/Controllers/CategoryController.cs
@@ -2,17 +2,39 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotnetPractice.Models.DTOs;
+using DotnetPractice.Models.Entities;
+using DotnetPractice.Models.Requests;
 using DotnetPractice.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DotnetPractice.Controllers
 {
-    public class CategoryController
+    [ApiController]
+    [Route("api/categories")]
+    public class CategoryController : ControllerBase
     {
         private readonly CategoryService _service;
 
         public CategoryController(CategoryService service)
         {
             _service = service;
+        }
+
+        [HttpGet("get-all")]
+        public async Task<ActionResult<ApiResponse<List<Category>>>> GetAllCategories()
+        {
+            ApiResponse<List<Category>> response = await _service.GetAllCategories();
+            return Ok(response);
+        }
+
+        [HttpPost("add")]
+        public async Task<ActionResult<ApiResponse<Category>>> AddCategory(
+            AddCategoryRequest request
+        )
+        {
+            ApiResponse<Category> response = await _service.AddCategory(request);
+            return Ok(response);
         }
     }
 }
