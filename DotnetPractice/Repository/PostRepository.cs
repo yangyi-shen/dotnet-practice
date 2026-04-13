d3b5417df319a2376cd4f1c4d420beebca970193
Yang-Yi Shen <mrshenyangyi@gmail.com>
2026-04-13 09:06:20 +0700

    feat: tạo hàm lớp truy cập dữ liệu cho posts

--- DotnetPractice/Repository/PostRepository.cs
+++ DotnetPractice/Repository/PostRepository.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotnetPractice.Models.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DotnetPractice.Repository
 {
@@ -12,6 +14,25 @@ namespace DotnetPractice.Repository
         public PostRepository(ContentDBContext dbContext)
         {
             _dbo = dbContext;
+        }
+
+        public async Task AddPost(Post data)
+        {
+            _dbo.Posts.Add(data);
+            await _dbo.SaveChangesAsync();
+        }
+
+        public async Task<List<Post>> GetFilteredPosts(Guid? userGUID, Guid? categoryGUID)
+        {
+            IQueryable<Post> query = _dbo.Posts;
+
+            if (userGUID != null)
+                query = query.Where(p => p.UserGUID == userGUID);
+
+            if (categoryGUID != null)
+                query = query.Where(p => p.CategoryGUID == categoryGUID);
+
+            return await query.ToListAsync();
         }
     }
 }
