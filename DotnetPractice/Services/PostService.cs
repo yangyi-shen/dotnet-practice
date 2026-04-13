3652ebf686b550723a7da1db038eb639b4646500
Yang-Yi Shen <mrshenyangyi@gmail.com>
2026-04-13 10:58:26 +0700

    feat: tạo api truy cập posts được lọc

--- DotnetPractice/Services/PostService.cs
+++ DotnetPractice/Services/PostService.cs
@@ -29,6 +29,27 @@ namespace DotnetPractice.Services
             _categoryRepository = categoryRepository;
         }
 
+        private enum GetFilteredPostsParam
+        {
+            USER,
+            CATEGORY,
+        }
+
+        public async Task<ApiResponse<List<Post>>> GetFilteredPosts(GetFilteredPostsRequest request)
+        {
+            Guid? userGUID = await validateAndParseGUID(
+                request.UserGUID,
+                GetFilteredPostsParam.USER
+            );
+            Guid? categoryGUID = await validateAndParseGUID(
+                request.CategoryGUID,
+                GetFilteredPostsParam.CATEGORY
+            );
+
+            List<Post> data = await _repository.GetFilteredPosts(userGUID, categoryGUID);
+            return new ApiResponse<List<Post>>(true, data);
+        }
+
         public async Task<ApiResponse<Post>> AddPost(AddPostRequest request)
         {
             Guid userGUID;
@@ -63,6 +84,39 @@ namespace DotnetPractice.Services
             await _repository.AddPost(data);
 
             return new ApiResponse<Post>(true, data);
+        }
+
+        private async Task<Guid?> validateAndParseGUID(string? rawGUID, GetFilteredPostsParam param)
+        {
+            Guid? GUID = null;
+
+            if (!string.IsNullOrEmpty(rawGUID))
+            {
+                try
+                {
+                    GUID = Guid.Parse(rawGUID);
+                }
+                catch (FormatException)
+                {
+                    throw new ApiException(ApiExceptions.PARAMETERS_INVALID);
+                }
+
+                if (param == GetFilteredPostsParam.USER)
+                {
+                    bool userExists = await _userRepository.UserExistsByGUID(GUID.Value);
+                    if (!userExists)
+                        throw new ApiException(ApiExceptions.USER_NOT_FOUND);
+                }
+                else if (param == GetFilteredPostsParam.CATEGORY)
+                {
+                    bool categoryExists = await _categoryRepository.CategoryExistsByGUID(
+                        GUID.Value
+                    );
+                    if (!categoryExists)
+                        throw new ApiException(ApiExceptions.CATEGORY_NOT_FOUND);
+                }
+            }
+            return GUID;
         }
     }
 }
